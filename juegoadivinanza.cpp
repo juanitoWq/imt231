0b08fbf74239e3b1b763c85e20707a4e6770f256
JHEYSON WILDER CUEVAS CHUGAR <jheyson.cuevas@ucb.edu.bo>
2024-09-17 09:19:01 -0400

    ejemplo

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std ;
+int main(){
+cout << "********************************************************"<<endl;
+cout << "*bienvenido al juego de la adivinanza *"<<endl;
+cout<< "*******************************************************"<<endl;
+}
