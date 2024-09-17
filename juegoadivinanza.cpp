1d456d44171ffcf7e94f392311fbf37651af1afd
JHEYSON WILDER CUEVAS CHUGAR <jheyson.cuevas@ucb.edu.bo>
2024-09-17 10:08:43 -0400

    2024-09-17_01 primera version juego adivinanza

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
 using namespace std ;
+include <iostream>
 int main(){
 cout << "********************************************************"<<endl;
 cout << "*bienvenido al juego de la adivinanza *"<<endl;
 cout<< "*******************************************************"<<endl;
+
+
 }
