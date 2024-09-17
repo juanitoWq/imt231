0a04a3b02914e08378f0a66cb4d276fd23998753
JHEYSON WILDER CUEVAS CHUGAR <jheyson.cuevas@ucb.edu.bo>
2024-09-17 10:14:04 -0400

    2024-09-17_02 segunda version incluye la variable numero_secreto

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,9 +1,10 @@
+
+#include <iostream>
 using namespace std ;
-include <iostream>
 int main(){
 cout << "********************************************************"<<endl;
 cout << "*bienvenido al juego de la adivinanza *"<<endl;
 cout<< "*******************************************************"<<endl;
-
-
+int numero_secreto = 42;
+cout<< "el numero secreto es ... " << numero_secreto << ". no lo digas a nadie! " <<endl;
 }
