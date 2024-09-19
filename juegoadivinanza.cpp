7d9d787e39356c7be186da10315ab84bbe95e482
JHEYSON WILDER CUEVAS CHUGAR <jheyson.cuevas@ucb.edu.bo>
2024-09-19 11:30:00 -0400

    prueba adivinanza

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,10 +1,27 @@
 
 #include <iostream>
-using namespace std ;
+using namespace std;
 int main(){
 cout << "********************************************************"<<endl;
 cout << "*bienvenido al juego de la adivinanza *"<<endl;
 cout<< "*******************************************************"<<endl;
 int numero_secreto = 42;
 cout<< "el numero secreto es ... " << numero_secreto << ". no lo digas a nadie! " <<endl;
+int adivina;
+cout<<" cual es el numero ? "<<endl;
+cin>> adivina;
+cout <<"el valor del numero es " << adivina<< endl;
+ if (adivina ==numero_secreto ){
+ cout <<"felicitaciones ... adivino el numero secreto"<<endl;
+ }
+
+ else if (adivina >numero_secreto) { 
+ cout<< "el numero mayor ingresado es mayor que el numero secreto "<<endl;
+ }
+
+ else  {
+ cout<<"el numero ingresado es menor que el numeor secreto "<<endl ;
+ }
+
 }
+
