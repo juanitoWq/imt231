42bcde17a63eb3059428f3c3a6b52a8d07baff2a
JHEYSON WILDER CUEVAS CHUGAR <jheyson.cuevas@ucb.edu.bo>
2024-09-24 08:52:36 -0400

    2024_09_24_01

---
+++ juego3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout << "********************************************************"<<endl;
+cout << "*bienvenido al juego de la adivinanza *"<<endl;
+cout<< "*******************************************************"<<endl;
+const int numero_secreto = 42;
+int adivina;
+bool no_acerto;
+while (no_acerto){
+bool acerto= adivina==numero_secreto ;
+bool mayor = adivina >numero_secreto;
+cout<<" cual es el numero ? "<<endl;
+cin>> adivina;
+cout <<"el valor del numero es " << adivina<< endl;
+
+ if (acerto ){
+ cout <<"felicitaciones ... adivino el numero secreto"<<endl;
+ no_acerto = false;
+ }
+
+ else if (mayor) { 
+ cout<< "el numero mayor ingresado es mayor que el numero secreto "<<endl;
+ }
+
+ else  {
+ cout<<"el numero ingresado es menor que el numeor secreto "<<endl ;
+ }
+
+}
+cout << "finalizo el juego "<<endl;
+}
